651b1dcfc9e9bf5d092f1607d587092c16e75808
Matias Grinberg <maticgrinberg@gmail.com>
2026-09-10 16:30:16 -0300

    fix: point Community button at the real Xindeler Discord
    Was https://xindeler.com/joinus/, an unconfirmed placeholder URL.

--- client/src/gui/components/logo_panel.rs
+++ client/src/gui/components/logo_panel.rs
@@ -29,7 +29,7 @@ impl LogoPanelComponent {
                         ))
                         .push(link_widget(
                             CHAT_ICON,
-                            "https://xindeler.com/joinus/",
+                            "https://discord.com/channels/1515805289339945181",
                             "Community",
                         ))
                         .push(link_widget(
